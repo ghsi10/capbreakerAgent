3bc61a9ad48a3445a69aa8711412c7b6515cdab8
ghsi10 <ghsi10@users.noreply.github.com>
2018-02-26 15:31:39 +0200

    Update capbreakerAgent.py

--- capbreakerAgent.py
+++ capbreakerAgent.py
@@ -1,15 +1,15 @@
 import pip
+import os
 from io import BytesIO
 from zipfile import ZipFile
 from time import sleep
+import subprocess
 
-# from threading import Thread
-
-username = "admin"
-password = "admin"
-server = "http://127.0.0.1"
-haschatUrl = "http://caprecovery.kuchi.be/test2.zip"
-hashcatLocation = "./hashcat"
+username = 'admin'
+password = 'admin'
+server = 'https://capbreaker.herokuapp.com'
+hashcatUrl = 'http://caprecovery.kuchi.be/test2.zip'
+hashcatLocation = './hashcat'
 mode = 3
 
 try:
@@ -19,64 +19,90 @@ except ImportError:
     import requests
 
 
-def downloadHashcat():
-    """Download hashcat"""
-    request = requests.get(haschatUrl)
-    zipFile = ZipFile(BytesIO(request.content))
-    zipFile.extractall()
-    zipFile.close()
+class Hashcat:
+    """ Hashcat class """
 
+    def __init__(self, location='./hashcat', url=''):
+        self.url = url
+        self.location = location
+        self.password = ''
+        self.foundPhrase = ''
+        if not os.path.isfile(self.location + '/hashcat64.exe'):
+            self.download()
 
-def saveHandshake(handshake):
-    """Save Handshake file in hashcatLocation"""
-    byteFile = bytearray.fromhex("484350580400000000")
-    essidLen = len(handshake['essid'])
-    byteFile += essidLen.to_bytes(1, byteorder='little')
-    byteFile += str.encode(handshake['essid'])
-    for i in range(essidLen, 32):
-        byteFile += bytearray.fromhex('00')
-    byteFile += bytearray.fromhex(handshake['keyVersion'])
-    byteFile += bytearray.fromhex(handshake['keyMic'])
-    byteFile += bytearray.fromhex(handshake['bssid'].replace(":", " "))
-    byteFile += bytearray.fromhex(handshake['anonce'])
-    byteFile += bytearray.fromhex(handshake['station'].replace(":", " "))
-    byteFile += bytearray.fromhex(handshake['snonce'])
-    eapolLen = int(len(handshake['eapol']) / 2)
-    byteFile += eapolLen.to_bytes(2, byteorder='little')
-    byteFile += bytearray.fromhex(handshake['eapol'])
-    for i in range(eapolLen, 256):
-        byteFile += bytearray.fromhex('00')
-    newFile = open(hashcatLocation + "/hs.hccapx", "wb")
-    newFile.write(byteFile)
-    newFile.close()
+    def download(self):
+        """ Download hashcat """
+        print('Downloading hashcat.')
+        request = requests.get(self.url)
+        zipFile = ZipFile(BytesIO(request.content))
+        zipFile.extractall(self.location)
+        zipFile.close()
+        print('Download completed.')
 
+    def handshake(self, handshake):
+        """ Save Handshake file in hashcatLocation """
+        byteFile = bytearray.fromhex('484350580400000000')
+        byteFile += len(handshake['essid']).to_bytes(1, byteorder='little')
+        byteFile += str.encode(handshake['essid'])
+        for i in range(len(handshake['essid']), 32):
+            byteFile += bytearray.fromhex('00')
+        byteFile += bytearray.fromhex(handshake['keyVersion'])
+        byteFile += bytearray.fromhex(handshake['keyMic'])
+        byteFile += bytearray.fromhex(handshake['bssid'].replace(':', ''))
+        byteFile += bytearray.fromhex(handshake['anonce'])
+        byteFile += bytearray.fromhex(handshake['station'].replace(':', ''))
+        byteFile += bytearray.fromhex(handshake['snonce'])
+        eapolLen = int(len(handshake['eapol']) / 2)
+        byteFile += eapolLen.to_bytes(2, byteorder='little')
+        byteFile += bytearray.fromhex(handshake['eapol'])
+        for i in range(eapolLen, 256):
+            byteFile += bytearray.fromhex('00')
+        newFile = open(self.location + '/hs.hccapx', 'wb')
+        newFile.write(byteFile)
+        newFile.close()
 
-if __name__ == "__main__":
-    # Login
-    response = requests.post(server + "/agent/login", auth=(username, password))
-    if response.status_code != 200 or response.text != "login success":
+    def scan(self, chunk):
+        """ Start scan with hashcat """
+        handshake = chunk['handshake']
+        self.handshake(handshake)
+        self.foundPhrase = (handshake['bssid'].replace(':', '') + ':').lower()
+        self.foundPhrase += (handshake['station'].replace(':', '') + ':').lower()
+        self.foundPhrase += handshake['essid']
+        process = subprocess.Popen(
+            'hashcat/hashcat64.exe -m 2500 -w 3 hashcat/hs.hccapx --force -a 3 05245?d?d?d', stdout=subprocess.PIPE)
         while True:
-            username = input("Enter your username: ")
-            password = input("Enter your password: ")
-            response = requests.post(server + "/agent/login", auth=(username, password))
-            if response.status_code == 200 and response.text == "login success":
-                print("login success")
+            output = process.stdout.readline().decode()
+            if not output:
+                print("Hashcat exeption.")
                 break
-            print("Invalid username and password.")
-    # Get task
-    while True:
-        downloadHashcat()
+            if 'Exhausted' in output or self.foundPhrase in output:
+                if self.foundPhrase in output:
+                    self.password = output.split(':')[4]
+                    process.kill()
+                requests.post(server + '/agent/setResult', headers={'uuid': chunk['uuid']},
+                              data={'password': hashcat.password},
+                              auth=(username, password))
+                break
 
-        response = requests.post(server + "/agent/getNextTask", auth=(username, password))
+
+if __name__ == '__main__':
+    print('Cap Breaker Agent.\n')
+    hashcat = Hashcat(hashcatLocation, hashcatUrl)
+    while True:
+        print('Looking for task.')
+        try:
+            response = requests.post(server + '/agent/getTask', auth=(username, password))
+        except:
+            print('Unable connect to server. please try again later.')
+            break
         if response.status_code == 200:
-            chunk = response.json()
-            saveHandshake(chunk['handshake'])
-            commands = chunk['commands']
-            exit(0)
-            sleep(10)
+            print('Task found, start scan...')
+            hashcat.scan(response.json())
+            print('Task done')
+            sleep(5)
         elif response.status_code == 204:
-            print("not found task")
-            sleep(30)
+            print('Not found task. Trying again in 60 seconds.')
+            sleep(60)
         else:
-            print("unexpected error occurred")
-            exit(0)
+            print('Unexpected error occurred.')
+            break
